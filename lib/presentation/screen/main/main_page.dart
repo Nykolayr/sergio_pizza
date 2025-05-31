6a579801b260abc9bdb2bb29a114494a57e529be
NykolayR <nykolayr@gmail.com>
2025-05-31 16:08:51 +0700

    add map

--- lib/presentation/screen/main/main_page.dart
+++ lib/presentation/screen/main/main_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('MainPage'));
+    return Scaffold(
+        body: Stack(
+      children: [
+        YandexMap(
+          onMapCreated: (controller) {
+            controller.moveCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                  target: Point(latitude: 55.751244, longitude: 37.618423),
+                  zoom: 15,
+                ),
+              ),
+            );
+          },
+          mapObjects: [
+            // Пример добавления метки на карту
+            PlacemarkMapObject(
+              mapId: const MapObjectId('placemark'),
+              point: Point(latitude: 55.751244, longitude: 37.618423),
+              icon: PlacemarkIcon.single(
+                PlacemarkIconStyle(
+                  image: BitmapDescriptor.fromAssetImage(
+                      'assets/svg/pickup_point.svg'),
+                  scale: 2,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
